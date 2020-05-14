177dd4232c9e2e863aa16d8623317e31f2a98e6c
ictwinfield <ictwinfield@gmail.com>
2020-05-15 09:26:57 +1200

    Made object for creating and drawing cubes.

--- Attila/Shapes.py
+++ Attila/Shapes.py
@@ -1,3 +1,9 @@
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+import numpy as np
+import pyrr
+from PIL import Image
+
 cube_vertices = [-0.5, -0.5,  0.5, 0.0, 0.0,
              0.5, -0.5,  0.5, 1.0, 0.0,
              0.5,  0.5,  0.5, 1.0, 1.0,
@@ -34,3 +40,58 @@ cube_indices = [ 0,  1,  2,  2,  3,  0,
            12, 13, 14, 14, 15, 12,
            16, 17, 18, 18, 19, 16,
            20, 21, 22, 22, 23, 20]
+
+
+def get_cube_vertices():
+    return np.array(cube_vertices, dtype=np.float32)
+
+def get_cube_indices():
+    return np.array(cube_indices, dtype=np.uint32)
+
+class Cube():
+    def __init__(self, vertices, indices, a_position, a_texture, offset, mod_loc, texture):
+        self.off_set = offset
+        self.a_pos = a_position
+        self.a_tex = a_texture
+        self.vertices = vertices
+        self.indices = indices
+        self.model_loc = mod_loc
+        self.texture = texture
+        self.pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+        self.VAO = glGenVertexArrays(1)
+        glBindVertexArray(self.VAO)
+
+        # Cube Vertex Buffer Object
+        self.VBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+
+        # Cube Element Buffer Object
+        self.EBO = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.EBO)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices.nbytes, self.indices, GL_STATIC_DRAW)
+        
+        glEnableVertexAttribArray(self.a_pos)
+        glVertexAttribPointer(self.a_pos, 3, GL_FLOAT, GL_FALSE, self.vertices.itemsize * 5, ctypes.c_void_p(0))
+
+        glEnableVertexAttribArray(self.a_tex)
+        glVertexAttribPointer(self.a_tex, 2, GL_FLOAT, GL_FALSE, self.vertices.itemsize * 5, ctypes.c_void_p(self.off_set))
+    
+    def draw(self):
+        glBindVertexArray(self.VAO)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, self.pos)
+        glDrawElements(GL_TRIANGLES, len(self.indices), GL_UNSIGNED_INT, None)
+    
+    def move(self, x, y, z):
+        self.x += x
+        self.y +=y
+        self.z += z
+        self.pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([self.x, self.y, self.z]))
+
+        
+
