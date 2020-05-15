bf7da8605b1ccabe399996cb16fce27f6cbeaf09
ictwinfield <ictwinfield@gmail.com>
2020-05-15 20:55:57 +1200

    Class 4 drawing lines & fnc 4 a grid of lines

---
+++ Attila/AT_vid13c.py
@@ -0,0 +1,134 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+import numpy as np
+import pyrr
+from TextureLoader import load_texture
+from Shapes import Cube, Line
+import Shapes
+
+
+vertex_src = """
+# version 330
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec2 a_texture;
+layout(location = 2) in vec3 a_color;
+uniform mat4 model;
+uniform mat4 projection;
+uniform mat4 view;
+out vec3 v_color;
+out vec2 v_texture;
+void main()
+{
+    gl_Position = projection * view * model * vec4(a_position, 1.0);
+    v_texture = a_texture;
+    v_color = a_color;
+}
+"""
+
+fragment_src = """
+# version 330
+in vec2 v_texture;
+in vec3 v_color;
+out vec4 out_color;
+uniform int switcher;
+uniform sampler2D s_texture;
+void main()
+{
+    if (switcher == 0){
+        out_color = texture(s_texture, v_texture);
+    }
+    else if (switcher == 1){
+        out_color = vec4(v_color, 1.0);   
+    }
+    
+}
+"""
+
+
+# glfw callback functions
+def window_resize(window, width, height):
+    glViewport(0, 0, width, height)
+    projection = pyrr.matrix44.create_perspective_projection_matrix(45, width / height, 0.1, 100)
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+
+# initializing glfw library
+if not glfw.init():
+    raise Exception("glfw can not be initialized!")
+
+# creating the window
+window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
+
+# check if window was created
+if not window:
+    glfw.terminate()
+    raise Exception("glfw window can not be created!")
+
+# set window's position
+glfw.set_window_pos(window, 400, 200)
+
+# set the callback function for window resize
+glfw.set_window_size_callback(window, window_resize)
+
+# make the context current
+glfw.make_context_current(window)
+
+cube_vertices = Shapes.get_cube_vertices()
+cube_indices = Shapes.get_cube_indices()
+
+line_vertices = Shapes.get_line_vertices()
+line_indices = Shapes.get_line_indices()
+
+ground_vertices = Shapes.get_ground_vertices(51)
+ground_indices = Shapes.get_ground_indices(51)
+
+shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
+model_loc = glGetUniformLocation(shader, "model")
+proj_loc = glGetUniformLocation(shader, "projection")
+view_loc = glGetUniformLocation(shader, "view")
+switcher_loc = glGetUniformLocation(shader, "switcher")
+
+textures = glGenTextures(2)
+
+cube_texture = load_texture("/home/melvin/Documents/Python/OpenGL/Attila/textures/crate.jpg", textures[0])
+quad_texture = load_texture("/home/melvin/Documents/Python/OpenGL/Attila/textures/brick.jpg", textures[1])
+
+################
+my_cube = Cube(cube_vertices, cube_indices, 0, 1, 12, model_loc, textures[0])
+my_cube.move(1, 0, 0)
+################
+my_ground = Line(ground_vertices, ground_indices, 0, 2, 12, model_loc)
+################
+
+glUseProgram(shader)
+glClearColor(0.2, 0.2, 0.2, 1)
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+projection = pyrr.matrix44.create_perspective_projection_matrix(45, 1280/720, 0.1, 100)
+
+# eye, target, up
+view = pyrr.matrix44.create_look_at(pyrr.Vector3([0, 2, 5]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 1, 0]))
+
+glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+
+# the main application loop
+while not glfw.window_should_close(window):
+    glfw.poll_events()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    my_cube.move(-0.004, 0, 0)
+    my_cube.draw(switcher_loc)
+    
+    my_ground.draw(switcher_loc)
+
+    rot_x = pyrr.Matrix44.from_x_rotation(0.5 * glfw.get_time())
+    rot_y = pyrr.Matrix44.from_y_rotation(0.8 * glfw.get_time())          
+
+    glfw.swap_buffers(window)
+
+# terminate glfw, free up allocated resources
+glfw.terminate()
--- Attila/Shapes.py
+++ Attila/Shapes.py
@@ -41,12 +41,35 @@ cube_indices = [ 0,  1,  2,  2,  3,  0,
            16, 17, 18, 18, 19, 16,
            20, 21, 22, 22, 23, 20]
 
+line_vertices = [-3.5, 0.0, 0.0, 0.0, 0.0, 1.0,
+                 3.5, 0.0, 0.0, 1.0, 1.0, 1.0]
+line_indices = [0, 1]
+
+def get_ground_vertices(n):
+    d = (n - 1) / 4
+    vertices = []
+    for i in range(n):
+        vertices += [i/2 - d, -0.5, d, 1.0, 1.0, 1.0]
+        vertices += [i/2 - d, -0.5, -d, 1.0, 1.0, 1.0]
+        vertices += [-d, -0.5, i/2 - d, 1.0, 1.0, 1.0]
+        vertices += [d, -0.5, i/2 - d, 1.0, 1.0, 1.0]
+    return np.array(vertices, dtype=np.float32)
+
+def get_ground_indices(n):
+    return np.array(list(range(n * 4)), dtype=np.uint32)
+        
 
 def get_cube_vertices():
     return np.array(cube_vertices, dtype=np.float32)
 
 def get_cube_indices():
     return np.array(cube_indices, dtype=np.uint32)
+
+def get_line_vertices():
+    return np.array(line_vertices, dtype=np.float32)
+
+def get_line_indices():
+    return np.array(line_indices, dtype=np.uint32)
 
 class Cube():
     def __init__(self, vertices, indices, a_position, a_texture, offset, mod_loc, texture):
@@ -81,7 +104,8 @@ class Cube():
         glEnableVertexAttribArray(self.a_tex)
         glVertexAttribPointer(self.a_tex, 2, GL_FLOAT, GL_FALSE, self.vertices.itemsize * 5, ctypes.c_void_p(self.off_set))
     
-    def draw(self):
+    def draw(self, switcher):
+        glUniform1i(switcher, 0)
         glBindVertexArray(self.VAO)
         glBindTexture(GL_TEXTURE_2D, self.texture)
         glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, self.pos)
@@ -94,4 +118,37 @@ class Cube():
         self.pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([self.x, self.y, self.z]))
 
         
+class Line():
+    def __init__(self, vertices, indices, a_position, a_color, offset, mod_loc):
+        self.off_set = offset
+        self.a_pos = a_position
+        self.a_col = a_color
+        self.vertices = vertices
+        self.indices = indices
+        self.model_loc = mod_loc
+        self.pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
 
+        self.VAO = glGenVertexArrays(1)
+        glBindVertexArray(self.VAO)
+
+        # Cube Vertex Buffer Object
+        self.VBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+
+        # Cube Element Buffer Object
+        self.EBO = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.EBO)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices.nbytes, self.indices, GL_STATIC_DRAW)
+        
+        glEnableVertexAttribArray(self.a_pos)
+        glVertexAttribPointer(self.a_pos, 3, GL_FLOAT, GL_FALSE, self.vertices.itemsize * 6, ctypes.c_void_p(0))
+
+        glEnableVertexAttribArray(self.a_col)
+        glVertexAttribPointer(self.a_col, 3, GL_FLOAT, GL_FALSE, self.vertices.itemsize * 6, ctypes.c_void_p(self.off_set))
+
+    def draw(self, switcher):
+        glUniform1i(switcher, 1)
+        glBindVertexArray(self.VAO)
+        glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, self.pos)
+        glDrawElements(GL_LINES, len(self.indices), GL_UNSIGNED_INT, None)
